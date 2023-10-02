9b09852424e23baa37d41d056d51564817be70ab
Flayger <36726488+Flayger@users.noreply.github.com>
2023-10-02 15:20:51 +0700

    First commit. Start auth

---
+++ src/main/java/com/flayger/SpringPetProject/SpringPetProjectApplication.java
@@ -0,0 +1,13 @@
+package com.flayger.SpringPetProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flayger/SpringPetProject/SpringPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flayger.SpringPetProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
